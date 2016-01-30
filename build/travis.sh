39ea37b997b7f6094d82df1d09f62c3ee0e9c91c
Pablo Couto <pablo@0x221e.net>
2016-01-30 22:28:29 +0100

    Travis: Remove bench tests

--- build/travis.sh
+++ build/travis.sh
@@ -4,11 +4,9 @@ set -ev
 
 if [ "${TRAVIS_RUST_VERSION}" = "nightly" ]; then
     travis-cargo build -- --no-default-features &&
-    travis-cargo test -- --no-default-features &&
-    travis-cargo bench -- --no-default-features
+    travis-cargo test -- --no-default-features
 else
     travis-cargo build &&
     travis-cargo test &&
-    travis-cargo bench &&
     travis-cargo --only stable doc
 fi
