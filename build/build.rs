65276ce2fa6307b00b4f1aaefcd618d09ec648ec
Pablo Couto <pablo@0x221e.net>
2016-01-30 17:05:30 +0100

    Add stable/nightly conditional compilation for `serde`

--- build/build.rs
+++ build/build.rs
@@ -1,17 +1,23 @@
-extern crate syntex;
-extern crate serde_codegen;
+#[cfg(default)]
+fn main() {
+    extern crate syntex;
+    extern crate serde_codegen;
 
-use std::env;
-use std::path::Path;
+    use std::env;
+    use std::path::Path;
 
-fn main () {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    pub fn main() {
+        let out_dir = env::var_os("OUT_DIR").unwrap();
 
-    let src = Path::new("build/type.rs.in");
-    let dst = Path::new(&out_dir).join("type.rs.out");
+        let src = Path::new("build/type.rs.in");
+        let dst = Path::new(&out_dir).join("type.rs.out");
 
-    let mut registry = syntex::Registry::new();
+        let mut registry = syntex::Registry::new();
 
-    serde_codegen::register(&mut registry);
-    registry.expand("", &src, &dst).unwrap();
+        serde_codegen::register(&mut registry);
+        registry.expand("", &src, &dst).unwrap();
+    }
 }
+
+#[cfg(not(default))]
+fn main() {}
