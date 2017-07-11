736bdcbed6ad0bded9ea7a4ec061fa7257d58385
Pablo Couto <pablo@uncharted.space>
2017-07-11 16:18:08 +0200

    Add exception handlers

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,8 @@
+use hyper;
 use std::convert::From;
 use std::error::Error as StdError;
 use std::fmt;
+use std::io;
 use std::result;
 use url;
 
@@ -8,6 +10,9 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
+    Hyper(hyper::Error),
+    UriError(hyper::error::UriError),
+    Io(io::Error),
     Url(url::ParseError),
     Unknown,
 }
@@ -15,17 +20,38 @@ pub enum Error {
 impl StdError for Error {
     fn description(&self) -> &str {
         match *self {
+            Error::Hyper(ref error) => error.description(),
+            Error::UriError(ref error) => error.description(),
+            Error::Io(ref error) => error.description(),
             Error::Url(ref error) => error.description(),
             Error::Unknown => "Unknown error",
         }
     }
 
-    // TODO: implement `fn cause`?
+    // TODO: Implement `fn cause`?
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_str(self.description())
+    }
+}
+
+impl From<hyper::Error> for Error {
+    fn from(error: hyper::Error) -> Self {
+        Error::Hyper(error)
+    }
+}
+
+impl From<hyper::error::UriError> for Error {
+    fn from(error: hyper::error::UriError) -> Self {
+        Error::UriError(error)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Error::Io(error)
     }
 }
 
